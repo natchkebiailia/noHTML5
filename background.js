5cf32b4785889cf8f96e946f116f64d7bf24ea8f
Natchkebia ILIA <nachkebiailia@yahoo.com>
2014-04-28 18:46:47 +0400

    user ager changed to chrome
    fixed bug at the beginning of video

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
         function(details) {
           for (var i = 0; i < details.requestHeaders.length; ++i) {
             if (details.requestHeaders[i].name === 'User-Agent') {
-              details.requestHeaders[i].value = 'Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US))';
+              details.requestHeaders[i].value = 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1';
               break;
             }
           }
